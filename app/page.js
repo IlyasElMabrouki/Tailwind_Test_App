399a03ae8ebad5d0d4452c97012b69572f510ea5
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2023-08-01 12:22:44 +0100

    added typography styles

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1 className="text-favcolor">
+    <h1 className="text-favcolor font-mono italic underline decoration-red-500 capitalize">
       Hello world!
     </h1>
   )
