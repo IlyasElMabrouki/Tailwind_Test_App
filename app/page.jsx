ee87347e3119583aad154bb429ccb6f3b4cfd982
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2023-09-11 21:33:49 +0100

    Apply Flex styles

--- app/page.jsx
+++ app/page.jsx
@@ -2,10 +2,10 @@ export default function Home() {
   return (
     <div>
       <div>Header</div>
-      <div className="flex">
-        <div className="basis-4/12 bg-red-100">SideBar 1</div>
-        <div className="basis-4/12 bg-red-200">SideBar 2</div>
-        <div className="basis-4/12 bg-red-300">SideBar 3</div>
+      <div className="flex items-baseline">
+        <div className="pt-2 pb-6 bg-sky-500">01</div>
+        <div className="pt-8 pb-12 bg-red-500">02</div>
+        <div className="pt-12 pb-4 bg-green-500">03</div>
       </div>
     </div>
   );
