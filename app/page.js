c95a3877897723fb76a14dc6aae24331e613e1ee
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2023-09-11 20:59:03 +0100

    Apply Flex basis

--- app/page.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <h1 className="text-favcolor font-mono italic underline decoration-red-500 capitalize">
-      Hello world!
-    </h1>
-  )
-}
---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div>
+      <div>Header</div>
+      <div className="flex">
+        <div className="basis-4/12 bg-red-100">SideBar 1</div>
+        <div className="basis-4/12 bg-red-200">SideBar 2</div>
+        <div className="basis-4/12 bg-red-300">SideBar 3</div>
+      </div>
+    </div>
+  );
+}
