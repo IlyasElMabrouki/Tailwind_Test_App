81ab4274ad6e7fe85eeefd212b975c57f7ac8505
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2023-09-13 21:40:30 +0100

    Grid utilities #2

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
     <div className="h-screen text-white bg-slate-900">
-      <div className="grid grid-cols-4 gap-1">
+      <div className="grid grid-rows-4 grid-flow-col gap-4">
         <div className="bg-sky-500 p-6 rounded-lg">First Element</div>
-        <div className="bg-red-500 p-6 rounded-lg col-span-2">Second Element</div>
-        <div className="bg-green-500 p-6 rounded-lg">Third Element</div>
+        <div className="bg-red-500 p-6 rounded-lg">Second Element</div>
+        <div className="bg-green-500 p-6 rounded-lg row-span-2">Third Element</div>
         <div className="bg-sky-500 p-6 rounded-lg">First Element</div>
-        <div className="bg-red-500 p-6 rounded-lg col-start-2 col-end-4">Second Element</div>
+        <div className="bg-red-500 p-6 rounded-lg row-span-2">Second Element</div>
         <div className="bg-green-500 p-6 rounded-lg">Third Element</div>
       </div>
     </div>
