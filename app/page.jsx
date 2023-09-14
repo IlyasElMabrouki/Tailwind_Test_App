b34c7137081fc03b8082544f7b0a3c0f6fec5fea
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2023-09-14 20:20:02 +0100

    Float utilities

--- app/page.jsx
+++ app/page.jsx
@@ -1,26 +1,32 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
     <div className="h-screen text-white bg-slate-900">
-      <div className="container mx-auto px-2">
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus
-          soluta voluptatem quo nulla excepturi iure totam accusantium, at et
-          cum. Praesentium quaerat tenetur ea tempora ipsum optio quibusdam
-          culpa illum.
-        </p>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus
-          soluta voluptatem quo nulla excepturi iure totam accusantium, at et
-          cum. Praesentium quaerat tenetur ea tempora ipsum optio quibusdam
-          culpa illum.
-        </p>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus
-          soluta voluptatem quo nulla excepturi iure totam accusantium, at et
-          cum. Praesentium quaerat tenetur ea tempora ipsum optio quibusdam
-          culpa illum.
-        </p>
-      </div>
+      <Image
+        className="float-left"
+        src="/shoes.jpg"
+        alt="image"
+        width={200}
+        height={200}
+      ></Image>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab error
+        accusamus eius consectetur, nam, veritatis aliquid amet adipisci quod
+        laboriosam esse maxime vel illum, hic voluptates deserunt obcaecati
+        asperiores magni. Lorem, ipsum dolor sit amet consectetur adipisicing
+        elit. Vero sit facere aperiam rerum. Inventore modi sunt pariatur?
+        Deleniti commodi illum dolores nulla saepe. Voluptate amet nostrum earum
+        ipsum numquam voluptatem molestiae reiciendis quis adipisci, inventore
+        architecto perspiciatis placeat ea a ipsa consequuntur eaque tenetur,
+        distinctio quaerat vero excepturi facilis! Nulla laudantium, laborum ab
+        quibusdam magnam obcaecati quidem commodi maiores expedita molestiae,
+        aperiam, animi aspernatur maxime beatae. Earum excepturi necessitatibus
+        consequatur optio cum nulla molestiae quaerat eligendi soluta! Ratione
+        dicta nobis ipsum earum optio fugit ipsa laboriosam tenetur obcaecati ab
+        consequuntur aliquid provident, non est quisquam, voluptatum odit ipsam
+        porro minima.
+      </p>
     </div>
   );
 }
