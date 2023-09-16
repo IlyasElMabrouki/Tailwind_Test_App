36c3f41eec56f735bba8be561a7d6586f6207f78
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2023-09-16 08:01:37 +0100

    Z-Index utilities

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,14 @@
 export default function Home() {
   return (
-    <div className="h-screen text-white bg-slate-900">
-      <div class="bg-red-500 relative w-32 h-32">
-        <div className="bg-sky-500 absolute w-16 h-16 top-16 right-0"></div>
+    <div className="flex h-screen text-white bg-slate-900">
+      <div className="z-20 rounded-full bg-pink-500 p-3 absolute top-0 left-0 border border-white">
+        <p className="text-center">03</p>
+      </div>
+      <div className="z-10 rounded-full bg-pink-500 p-3 absolute top-0 left-10 border border-white">
+        <p className="text-center">02</p>
+      </div>
+      <div className="z-0 rounded-full bg-pink-500 p-3 absolute top-0 left-20 border border-white">
+        <p className="text-center">01</p>
       </div>
     </div>
   );
