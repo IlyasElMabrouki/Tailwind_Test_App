ff2caebf62e4a3e14ce78ef176d76843646bf469
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2023-09-16 07:20:25 +0100

    Position utilities

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,8 @@
 export default function Home() {
   return (
     <div className="h-screen text-white bg-slate-900">
-      <div class="bg-red-500">
-        <span className="bg-sky-500 inline-block w-100 h-32 m-5">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam,
-          voluptate.
-        </span>
-        <span className="bg-sky-500 inline-block w-100 h-32 m-5">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam,
-          voluptate.
-        </span>
+      <div class="bg-red-500 relative w-32 h-32">
+        <div className="bg-sky-500 absolute w-16 h-16 top-16 right-0"></div>
       </div>
     </div>
   );
