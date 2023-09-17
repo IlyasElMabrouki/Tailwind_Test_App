facaa183449b9fc1839f8622c142a77b0dc40563
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2023-09-17 10:39:42 +0100

    Animation utilities

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex h-screen text-white bg-slate-900">
-      <div className="z-20 rounded-full bg-pink-500 p-3 absolute top-0 left-0 border border-white">
-        <p className="text-center">03</p>
-      </div>
-      <div className="z-10 rounded-full bg-pink-500 p-3 absolute top-0 left-10 border border-white">
-        <p className="text-center">02</p>
-      </div>
-      <div className="z-0 rounded-full bg-pink-500 p-3 absolute top-0 left-20 border border-white">
-        <p className="text-center">01</p>
-      </div>
+    <div className="h-screen text-black">
+      <div className="h-32 w-32 shadow-lg shadow-red-500/50 m-auto mt-5 hover:scale-50 transition"></div>
     </div>
   );
 }
